d086da62ff3ea277e2dd7d74b64f9d60070b2cc1
PJ Passalacqua <pjpassa@gmail.com>
2015-06-12 11:29:19 -0400

    stubbed out methods and wrote out init

--- simulation.py
+++ simulation.py
@@ -1,1 +1,36 @@
-__author__ = 'pjpassalacqua'
+import random
+import numpy as np
+
+
+class Simulation:
+
+    def __init__(self, road, drivers, car_types, tick_interval=1):
+        self.road = road
+        self.drivers = drivers
+        num_cars = int(self.road.length * 30 / 1000)
+        random.shuffle(car_types)
+        self.car_list = []
+        total_cars = 0
+        for percent_car in range(len(car_types)):
+            percent_car[0] *= num_cars
+            total_cars += int(percent_car[0])
+        for _ in range(num_cars - total_cars):
+            car_types[random.randint(0, len(car_types) - 1)][0] += 1
+        for car_count, car_class in car_types:
+            for _ in range(car_count):
+                self.car_list.append(car_class())
+        random.shuffle(self.car_list)
+        for position, car in enumerate(self.car_list):
+            car.location = int(position * road.length / total_cars)
+        self.tick_interval = tick_interval
+
+
+    def run(self):
+        pass
+
+    def update(self):
+        pass
+
+    @property
+    def current_state(self):
+        pass
